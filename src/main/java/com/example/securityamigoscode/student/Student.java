e5e6de48b57f89c3962014851ead45f9aa5d8552
salexsh <ShutovAlexandr@yandex.ru>
2022-09-30 11:12:48 +0300

    Added classes

---
+++ src/main/java/com/example/securityamigoscode/student/Student.java
@@ -0,0 +1,16 @@
+package com.example.securityamigoscode.student;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+public class Student {
+
+    private final Integer studentId;
+    private final String studentName;
+
+    public Student(Integer studentId, String studentName) {
+        this.studentId = studentId;
+        this.studentName = studentName;
+    }
+}
