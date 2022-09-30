b0c00de8be10224cd2c505a5fb753bd4d8002875
salexsh <ShutovAlexandr@yandex.ru>
2022-09-30 11:00:47 +0300

    Initial commit

---
+++ src/test/java/com/example/securityamigoscode/SecurityAmigoscodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.securityamigoscode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityAmigoscodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
