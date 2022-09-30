7ce947c34ce49c3cb29e8adc639076e302f14a45
salexsh <ShutovAlexandr@yandex.ru>
2022-09-30 14:47:32 +0300

    Added permissions for an admin role

--- src/main/java/com/example/securityamigoscode/security/ApplicationUserRole.java
+++ src/main/java/com/example/securityamigoscode/security/ApplicationUserRole.java
@@ -1,6 +1,26 @@
 package com.example.securityamigoscode.security;
 
+import com.google.common.collect.Sets;
+import lombok.Getter;
+
+import java.util.Set;
+
+import static com.example.securityamigoscode.security.ApplicationUserPermission.*;
+
+@Getter
 public enum ApplicationUserRole {
-    STUDENT,
-    ADMIN
+    STUDENT(Sets.newHashSet()),
+    ADMIN(Sets.newHashSet(
+            COURSE_READ,
+            COURSE_WRITE,
+            STUDENT_READ,
+            STUDENT_WRITE
+    ));
+
+    private final Set<ApplicationUserPermission> permissions;
+
+    ApplicationUserRole(Set<ApplicationUserPermission> permissions) {
+        this.permissions = permissions;
+    }
+
 }
