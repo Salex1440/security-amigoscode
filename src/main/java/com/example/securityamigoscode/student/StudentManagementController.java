32a791a2d1d5e0909cec176083bc17f4bfc849a4
salexsh <ShutovAlexandr@yandex.ru>
2022-09-30 15:30:51 +0300

    Added a new class

---
+++ src/main/java/com/example/securityamigoscode/student/StudentManagementController.java
@@ -0,0 +1,9 @@
+package com.example.securityamigoscode.student;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("management/api/v1/students")
+public class StudentManagementController {
+}
