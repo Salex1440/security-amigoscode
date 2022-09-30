c729eb99608828214ac908cb51f04f1fff8fc409
salexsh <ShutovAlexandr@yandex.ru>
2022-09-30 11:18:42 +0300

    Added business logic

--- src/main/java/com/example/securityamigoscode/student/StudentController.java
+++ src/main/java/com/example/securityamigoscode/student/StudentController.java
@@ -5,12 +5,25 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.List;
+
 @RestController
 @RequestMapping(path= "api/v1/students")
 public class StudentController {
 
+    private static final List<Student> STUDENTS = Arrays.asList(
+            new Student(1, "James Bond"),
+            new Student(2, "Maria Jones"),
+            new Student(3, "Adam Smith")
+
+    );
+
     @GetMapping(path="/{studentId}")
     public Student getStudent(@PathVariable("studentId") Integer studentId) {
-
+        return STUDENTS.stream()
+                .filter((student -> studentId.equals(student.getStudentId())))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("Student " + studentId + " doesn't exist!"));
     }
 }
