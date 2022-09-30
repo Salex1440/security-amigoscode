118c54b23b9877d29ef7a3f4473d800a080a75b4
salexsh <ShutovAlexandr@yandex.ru>
2022-09-30 12:18:52 +0300

    Added basic authentication

--- src/main/java/com/example/securityamigoscode/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/securityamigoscode/security/ApplicationSecurityConfig.java
@@ -11,7 +11,12 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-
+        http
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
     }
 
 }
