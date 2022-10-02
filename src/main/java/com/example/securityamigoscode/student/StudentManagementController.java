41b3b274d5c525b7fb126e31c39d7192bfb44c04
salexsh <ShutovAlexandr@yandex.ru>
2022-10-02 19:40:24 +0300

    Added preauthority annotations

--- src/main/java/com/example/securityamigoscode/student/StudentManagementController.java
+++ src/main/java/com/example/securityamigoscode/student/StudentManagementController.java
@@ -1,5 +1,6 @@
 package com.example.securityamigoscode.student;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,21 +18,25 @@ public class StudentManagementController {
     );
 
     @GetMapping
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_ADMINTRAINEE')")
     public List<Student> getAllStudents() {
         return STUDENTS;
     }
 
     @PostMapping
+    @PreAuthorize("hasAuthority('student:write')")
     public void registerNewStudent(@RequestBody Student student) {
         System.out.println(student.toString());
     }
 
     @DeleteMapping(path = "{studentId}")
+    @PreAuthorize("hasAuthority('student:write')")
     public void deleteStudent(@PathVariable("studentId") Integer studentId) {
         System.out.println(studentId.toString());
     }
 
     @PutMapping(path = "{studentId}")
+    @PreAuthorize("hasAuthority('student:write')")
     public void updateStudent(@PathVariable("studentId") Integer studentId,
                               @RequestBody Student student) {
         System.out.println(String.format("%s %s", studentId, student.toString()));
