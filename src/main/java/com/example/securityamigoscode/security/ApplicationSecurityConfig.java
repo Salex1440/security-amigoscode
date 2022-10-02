e788ec32e28ab781de2a43291d1d800067ad8fb2
salexsh <ShutovAlexandr@yandex.ru>
2022-10-02 19:44:33 +0300

    Deleted comments

--- src/main/java/com/example/securityamigoscode/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/securityamigoscode/security/ApplicationSecurityConfig.java
@@ -3,7 +3,6 @@ package com.example.securityamigoscode.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -14,7 +13,6 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
-import static com.example.securityamigoscode.security.ApplicationUserPermission.COURSE_WRITE;
 import static com.example.securityamigoscode.security.ApplicationUserRole.*;
 
 @Configuration
@@ -36,10 +34,6 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers("/", "/index", "/css/*", "/js/*").permitAll()
                 .antMatchers("/api/**").hasRole(STUDENT.name())
-//                .antMatchers(HttpMethod.DELETE,"/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
-//                .antMatchers(HttpMethod.POST,"/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
-//                .antMatchers(HttpMethod.PUT,"/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
-//                .antMatchers(HttpMethod.GET,"/management/api/**").hasAnyRole(ADMIN.name(), ADMINTRAINEE.name())
                 .anyRequest()
                 .authenticated()
                 .and()
@@ -53,19 +47,16 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .username("annasmith")
                 .password(passwordEncoder.encode("pass"))
                 .authorities(STUDENT.getGrantedAuthorities())
-//                .roles(STUDENT.name()) // ROLE_STUDENT
                 .build();
         UserDetails salexUser = User.builder()
                 .username("salex")
                 .password(passwordEncoder.encode("strongPass"))
                 .authorities(ADMIN.getGrantedAuthorities())
-//                .roles(ADMIN.name()) // ROLE_ADMIN
                 .build();
         UserDetails tomUser = User.builder()
                 .username("tom")
                 .password(passwordEncoder.encode("strongPass"))
                 .authorities(ADMINTRAINEE.getGrantedAuthorities())
-//                .roles(ADMINTRAINEE.name()) // ROLE_ADMINTRAINEE
                 .build();
         return new InMemoryUserDetailsManager(
                 annaSmithUser,
