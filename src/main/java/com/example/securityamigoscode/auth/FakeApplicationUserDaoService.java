91cad19866d6d81c6701bc6e0244c5fae3ce152d
Salex <123@13.com>
2022-10-03 16:37:17 +0300

    Implemented a fake repository

--- src/main/java/com/example/securityamigoscode/auth/FakeApplicationUserDaoService.java
+++ src/main/java/com/example/securityamigoscode/auth/FakeApplicationUserDaoService.java
@@ -3,12 +3,14 @@ package com.example.securityamigoscode.auth;
 import com.google.common.collect.Lists;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
 import static com.example.securityamigoscode.security.ApplicationUserRole.*;
 
+@Repository("fake")
 public class FakeApplicationUserDaoService implements ApplicationUserDao {
 
     private final PasswordEncoder passwordEncoder;
@@ -20,7 +22,10 @@ public class FakeApplicationUserDaoService implements ApplicationUserDao {
 
     @Override
     public Optional<ApplicationUser> selectApplicationUserByUsername(String username) {
-        return Optional.empty();
+        return getApplicationUsers()
+                .stream()
+                .filter(applicationUser -> username.equals(applicationUser.getUsername()))
+                .findFirst();
     }
 
     private List<ApplicationUser> getApplicationUsers() {
