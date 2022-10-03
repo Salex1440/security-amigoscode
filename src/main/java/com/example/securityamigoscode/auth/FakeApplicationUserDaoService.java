5012c357ce497ba1a152d9ed316fbcec4e7d8cc6
Salex <123@13.com>
2022-10-03 16:33:47 +0300

    Added Users to the FakeApplicationUserDaoService

--- src/main/java/com/example/securityamigoscode/auth/FakeApplicationUserDaoService.java
+++ src/main/java/com/example/securityamigoscode/auth/FakeApplicationUserDaoService.java
@@ -1,11 +1,59 @@
 package com.example.securityamigoscode.auth;
 
+import com.google.common.collect.Lists;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.List;
 import java.util.Optional;
 
+import static com.example.securityamigoscode.security.ApplicationUserRole.*;
+
 public class FakeApplicationUserDaoService implements ApplicationUserDao {
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public FakeApplicationUserDaoService(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
 
     @Override
     public Optional<ApplicationUser> selectApplicationUserByUsername(String username) {
         return Optional.empty();
     }
+
+    private List<ApplicationUser> getApplicationUsers() {
+        List<ApplicationUser> applicationUsers = Lists.newArrayList(
+                new ApplicationUser(
+                        "annasmith",
+                        passwordEncoder.encode("pass"),
+                        STUDENT.getGrantedAuthorities(),
+                        true,
+                        true,
+                        true,
+                        true
+                ),
+                new ApplicationUser(
+                        "linda",
+                        passwordEncoder.encode("pass"),
+                        ADMIN.getGrantedAuthorities(),
+                        true,
+                        true,
+                        true,
+                        true
+                ),
+                new ApplicationUser(
+                        "tom",
+                        passwordEncoder.encode("pass"),
+                        ADMINTRAINEE.getGrantedAuthorities(),
+                        true,
+                        true,
+                        true,
+                        true
+                )
+        );
+        return applicationUsers;
+    }
+
 }
