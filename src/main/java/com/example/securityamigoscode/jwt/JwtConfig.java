fe88486e8023c75d2088be311f955614825142ee
Salex <123@13.com>
2022-10-05 15:56:38 +0300

    Created Jwt config class

---
+++ src/main/java/com/example/securityamigoscode/jwt/JwtConfig.java
@@ -0,0 +1,32 @@
+package com.example.securityamigoscode.jwt;
+
+import com.google.common.net.HttpHeaders;
+import io.jsonwebtoken.security.Keys;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+import javax.crypto.SecretKey;
+
+@Getter
+@Setter
+@ConfigurationProperties(prefix = "application.jwt")
+public class JwtConfig {
+
+    private String secretKey;
+    private String tokenPrefix;
+    private Integer tokenExpirationAfterDays;
+
+    public JwtConfig() {
+    }
+
+    @Bean
+    public SecretKey getSecretKeyForSigning() {
+        return Keys.hmacShaKeyFor(secretKey.getBytes());
+    }
+
+    public String getAuthorizationHeader() {
+        return HttpHeaders.AUTHORIZATION;
+    }
+}
